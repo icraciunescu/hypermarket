df6f4cac44e1993e2c79285160841c02c676c2df
Ion Craciunescu <icraciunescu@hotmail.com>
2019-03-03 15:39:19 +0200

    project structure

---
+++ hypermarket-core/src/main/java/ro/sda/hypermarket/core/entity/Supplier.java
@@ -0,0 +1,70 @@
+package ro.sda.hypermarket.core.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "suppliers", schema = "hypermarket")
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 40, nullable = false)
+    private String name;
+
+    @Column(name = "contact_no", length = 40, nullable = false)
+    private String contact_no;
+
+    @Column(name = "city", length = 40, nullable = false)
+    private String city;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact_no() {
+        return contact_no;
+    }
+
+    public void setContact_no(String contact_no) {
+        this.contact_no = contact_no;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Supplier supplier = (Supplier) o;
+        return id.equals(supplier.id) &&
+                name.equals(supplier.name) &&
+                contact_no.equals(supplier.contact_no) &&
+                city.equals(supplier.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, contact_no, city);
+    }
+}
