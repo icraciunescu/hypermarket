beedc480c9dc304e09abbd6bea02735d96c2e2c6
Ion Craciunescu <icraciunescu@hotmail.com>
2019-03-23 14:52:35 +0200

    ProductDao

--- hypermarket-core/src/main/java/ro/sda/hypermarket/core/dao/ProductCategoryDaoImpl.java
+++ hypermarket-core/src/main/java/ro/sda/hypermarket/core/dao/ProductCategoryDaoImpl.java
@@ -4,12 +4,15 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import ro.sda.hypermarket.core.entity.ProductCategory;
-
 import javax.persistence.criteria.CriteriaQuery;
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
+@Transactional
 public class ProductCategoryDaoImpl  implements ProductCategoryDao {
 
     private List<ProductCategory> productCategory = new ArrayList<>();
--- hypermarket-core/src/main/java/ro/sda/hypermarket/core/dao/ProductDaoImpl.java
+++ hypermarket-core/src/main/java/ro/sda/hypermarket/core/dao/ProductDaoImpl.java
@@ -1,4 +1,69 @@
 package ro.sda.hypermarket.core.dao;
 
-public class ProductDaoImpl {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ro.sda.hypermarket.core.entity.Product;
+import ro.sda.hypermarket.core.entity.ProductCategory;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@Transactional
+public class ProductDaoImpl   implements ProductDao {
+
+    private List<Product> products = new ArrayList<>();
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    @Override
+    public Product getById(Long id) {
+        return getCurrentSession().byId(Product.class).getReference(id);
+    }
+
+    @Override
+    public List<Product> getAll() {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaQuery<Product> criteriaQuery = session.getCriteriaBuilder().createQuery(Product.class);
+        criteriaQuery.from(ProductCategory.class);
+        List<Product> allProduct = session.createQuery(criteriaQuery).getResultList();
+        return allProduct;
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        getCurrentSession().save(product);
+        return product;
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        Transaction tr = sessionFactory.getCurrentSession().beginTransaction();
+        Product product1 = getById(product.getId());
+        sessionFactory.getCurrentSession().merge(product1);
+        sessionFactory.getCurrentSession().flush();
+        tr.commit();
+        return product1;
+    }
+
+    @Override
+    public void deleteProduct(Product product) {
+        Transaction tr = sessionFactory.getCurrentSession().beginTransaction();
+        Product product1 = getById(product.getId());
+        sessionFactory.getCurrentSession().delete(product1);
+        sessionFactory.getCurrentSession().flush();
+        tr.commit();
+    }
 }
+
+
