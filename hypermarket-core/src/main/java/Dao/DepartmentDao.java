5498a8180d74d3d59d1676c533962fe96f3c2b52
Ion Craciunescu <icraciunescu@hotmail.com>
2019-03-23 11:32:27 +0200

    SupplierDaoImpl

--- hypermarket-core/src/main/java/Dao/DepartmentDao.java
+++ hypermarket-core/src/main/java/Dao/DepartmentDao.java
@@ -1,4 +1,17 @@
 package Dao;
 
-public class DepartmentDao {
+        import org.hibernate.Session;
+        import ro.sda.hypermarket.core.entity.Department;
+
+        import java.util.List;
+
+public interface DepartmentDao {
+
+    Session getCurrentSession();
+
+    Department getById(Long id);
+    List<Department> getAll();
+    Department createDeparment(Department department);
+    void updateDepartment(Department department);
+    void deleteSupplier(Department department);
 }
--- hypermarket-core/src/main/java/Dao/DepartmentDaoImpl.java
+++ hypermarket-core/src/main/java/Dao/DepartmentDaoImpl.java
@@ -1,17 +1,4 @@
 package Dao;
 
-        import org.hibernate.Session;
-        import ro.sda.hypermarket.core.entity.Department;
-
-        import java.util.List;
-
-public interface DepartmentDaoImpl {
-
-    Session getCurrentSession();
-
-    Department getById(Long id);
-    List<Department> getAll();
-    Department createDeparment(Department department);
-    void updateDepartment(Department department);
-    void deleteSupplier(Department department);
+public class DepartmentDaoImpl {
 }
