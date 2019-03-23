5093dcfcd7d9290b2641773c739a91dd861f1237
@sidorb11 <sidorb@yahoo.com>
2019-03-23 11:34:18 +0200

    Merge branch 'master' of D:\WS\hypermarket\hypermarket with conflicts.

--- hypermarket-core/src/main/java/Dao/DepartmentDao.java
+++ hypermarket-core/src/main/java/Dao/DepartmentDao.java
@@ -1,17 +1,17 @@
 package Dao;
 
-        import org.hibernate.Session;
-        import ro.sda.hypermarket.core.entity.Department;
+import org.hibernate.Session;
+import ro.sda.hypermarket.core.entity.Department;
+import ro.sda.hypermarket.core.entity.Supplier;
 
-        import java.util.List;
+import java.util.List;
 
 public interface DepartmentDao {
-
     Session getCurrentSession();
 
-    Department getById(Long id);
+    Supplier getById(Long id);
     List<Department> getAll();
-    Department createDeparment(Department department);
+    Supplier createDeparment(Department department);
     void updateDepartment(Department department);
-    void deleteSupplier(Department department);
+    void deleteDepartment(Department department);
 }
--- hypermarket-core/src/main/java/Dao/DepartmentDaoImpl.java
+++ hypermarket-core/src/main/java/Dao/DepartmentDaoImpl.java
@@ -1,4 +1,72 @@
 package Dao;
 
-public class DepartmentDaoImpl {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ro.sda.hypermarket.core.entity.Department;
+import ro.sda.hypermarket.core.entity.Supplier;
+
+import javax.persistence.criteria.CriteriaQuery;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@Transactional
+public class DepartmentDaoImpl implements DepartmentDao {
+
+    private List<Department> departments= new ArrayList<>();
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Session getCurrentSession(){
+        return sessionFactory.getCurrentSession();
+    }
+
+    @Override
+    public Department getById(Long id) {
+        return getCurrentSession().byId(Department.class).getReference(id);
+    }
+
+    @Override
+    public List<Department> getAll() {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaQuery<Department> criteriaQuery = session.getCriteriaBuilder().createQuery(Department.class);
+        criteriaQuery.from(Department.class);
+        List<Department> allDepartment = session.createQuery(criteriaQuery).getResultList();
+        return allDepartment;
+    }
+
+//    @Override
+//    public Department createDeparment(Department department) {
+//        return Department;
+//    }
+
+    @Override
+    public Department createDepartment(Department department) {
+        getCurrentSession().save(department);
+        return department;
+    }
+
+    @Override
+    public void updateDepartment(Department department) {
+        Transaction tr = sessionFactory.getCurrentSession().beginTransaction();
+        Department supplier1 = getById(department.getId());
+        sessionFactory.getCurrentSession().merge(supplier1);
+        sessionFactory.getCurrentSession().flush();
+        tr.commit();
+    }
+
+    @Override
+    public void deleteDepartment(Department supplier) {
+        Transaction tr = sessionFactory.getCurrentSession().beginTransaction();
+        Department supplier1 = getById(supplier.getId());
+        sessionFactory.getCurrentSession().delete(supplier1);
+        sessionFactory.getCurrentSession().flush();
+        tr.commit();
+    }
 }
