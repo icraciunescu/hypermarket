023f39f1f05d4f180fe0aec41866d1f3f1389240
Ion Craciunescu <icraciunescu@hotmail.com>
2019-03-23 12:10:06 +0200

    DepartmentDaoImpl v2

---
+++ hypermarket-core/src/main/java/Dao/EmployeeDao.java
@@ -0,0 +1,23 @@
+package Dao;
+
+import org.hibernate.Session;
+import ro.sda.hypermarket.core.entity.Department;
+import ro.sda.hypermarket.core.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+
+    Session getCurrentSession();
+
+    Department getById(Long id);
+
+    List<Department> getAll();
+
+    Department createEmployee(Employee employee);
+
+    void updateEmployee(Employee employee);
+
+    void deleteEmployee(Employee employee);
+
+}
