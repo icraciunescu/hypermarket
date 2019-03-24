6c6d007c257b7dd3e49d411b5c2bf5ac8f1ec255
Ion Craciunescu <icraciunescu@hotmail.com>
2019-03-24 13:48:27 +0200

    product category service impl

---
+++ hypermarket-core/src/main/java/ro/sda/hypermarket/core/service/ProductCategoryServiceImpl.java
@@ -0,0 +1,50 @@
+package ro.sda.hypermarket.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ro.sda.hypermarket.core.dao.ProductCategoryDao;
+import ro.sda.hypermarket.core.entity.ProductCategory;
+import ro.sda.hypermarket.core.repository.ProductCategoryRepository;
+import java.util.List;
+
+
+@Service("productCategoryService")
+@Transactional(readOnly = true, rollbackFor = Exception.class)
+public class ProductCategoryServiceImpl implements ProductCategoryService {
+
+    @Autowired
+    private ProductCategoryDao productCategoryDao;
+
+    @Autowired
+    private ProductCategoryRepository productCategoryRepository;
+
+    @Override
+    public ProductCategory getById(Long id) {
+        return productCategoryDao.getById(id);
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        return productCategoryDao.getAll();
+    }
+
+    @Override
+    @Transactional
+    public ProductCategory createProductCategory(ProductCategory productCategory, boolean useHibernate) {
+        if (useHibernate) {
+            return productCategoryDao.createProductCategory(productCategory);
+        }
+        return productCategoryRepository.save(productCategory);
+    }
+
+    @Override
+    public ProductCategory updateProductCategory(ProductCategory productCategory) {
+        return productCategoryDao.updateProductCategory(productCategory);
+    }
+
+    @Override
+    public void deleteProductCategory(ProductCategory productCategory) {
+
+    }
+}
