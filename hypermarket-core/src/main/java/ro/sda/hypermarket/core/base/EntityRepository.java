2f5c5310758392a5c0d6d99563ee471bf3c6ec5a
@sidorb11 <sidorb@yahoo.com>
2019-03-24 10:13:52 +0200

    Entity repository

--- hypermarket-core/src/main/java/ro/sda/hypermarket/core/base/EntityRepository.java
+++ hypermarket-core/src/main/java/ro/sda/hypermarket/core/base/EntityRepository.java
@@ -3,4 +3,5 @@ package ro.sda.hypermarket.core.base;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EntityRepository<T extends BaseEntity>extends JpaRepository<T, Long> {
+    public T findById(long id);
 }
